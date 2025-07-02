4566edfd70bf3db9ad8351788cc0008dfe8764ec
Mohamed-Amine-Affes <mohamedamineaaffes@gmail.com>
2025-07-02 22:32:10 +0000

    modified test script

--- tests/script_stage.py
+++ tests/script_stage.py
@@ -1,22 +1,11 @@
 import pytest
-import pandas as pd
 import numpy as np
 from recommender import (
-    load_data,
-    weighted_rating,
-    build_chart,
-    get_recommendations,
-    get_director,
-    filter_keywords,
-    improved_recommendations,
     convert_int,
-    hybrid,
+    get_director,
+    weighted_rating,
 )
 
-@pytest.fixture(scope="module")
-def data():
-    # Load the data only once for all tests
-    return load_data()
 
 def test_convert_int():
     assert convert_int("10") == 10
@@ -27,14 +16,12 @@ def test_get_director():
     assert get_director(crew) == "James Cameron"
     assert np.isnan(get_director([{"job": "Producer", "name": "Someone"}]))
 
-
 def test_weighted_rating():
     x = {"vote_count": 200, "vote_average": 8.5}
     m = 100
     C = 7.0
     wr = weighted_rating(x, m, C)
     assert wr > 0
-
 
 if __name__ == "__main__":
     pytest.main()
