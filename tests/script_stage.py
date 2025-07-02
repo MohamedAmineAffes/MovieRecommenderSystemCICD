f6dd5d9b669e4ab0cfef804026a0e8cb853ef063
Mohamed-Amine-Affes <mohamedamineaaffes@gmail.com>
2025-07-02 23:48:54 +0000

    fixing memory issues

--- tests/script_stage.py
+++ tests/script_stage.py
@@ -1,20 +1,8 @@
 import pytest
-import numpy as np
 from recommender import (
-    convert_int,
-    get_director,
+
     weighted_rating,
 )
-
-
-def test_convert_int():
-    assert convert_int("10") == 10
-    assert np.isnan(convert_int("not_a_number"))
-
-def test_get_director():
-    crew = [{"job": "Director", "name": "James Cameron"}, {"job": "Producer", "name": "Someone"}]
-    assert get_director(crew) == "James Cameron"
-    assert np.isnan(get_director([{"job": "Producer", "name": "Someone"}]))
 
 def test_weighted_rating():
     x = {"vote_count": 200, "vote_average": 8.5}
